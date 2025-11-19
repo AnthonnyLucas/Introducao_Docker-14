a0b4fbc46e580f513701d2f37740c9c6dd59795c
Anthonny Lucas <119464268+AnthonnyLucas@users.noreply.github.com>
2025-11-19 01:15:01 -0300

    Add files via upload

---
+++ 14/process.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+print("--- Script de Processamento Iniciado ---")
+print(f"Versão do Python: {sys.version}")
+print(f"Diretório de trabalho atual: {os.getcwd()}")
+print("Processing data...")
+print("--- Script de Processamento Concluído ---")
